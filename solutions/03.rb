39e0dfa14dff83b26e6af31b4ac09e17829bc2f3
Gabriella Luhova <gabriela.luhova@gmail.com>
2015-01-18 22:55:37 +0200

    Update solution to task 3

--- solutions/03.rb
+++ solutions/03.rb
@@ -1,76 +1,133 @@
 module RBFS
-  class File
-    attr_accessor :data
-
-    def initialize(object = nil)
-      @data = object
+  class Parser
+    def initialize(string_data)
+      @data = string_data
     end
 
-    def data_type
-      case @data
-      when nil        then :nil
-      when String     then :string
-      when Numeric    then :number
-      when Symbol     then :symbol
-      else                 :boolean
+    def each
+      size = read_next_record.to_i
+
+      size.times do
+        entity_name   = read_next_record
+        entity_string = read_next_entity
+
+        yield entity_name, entity_string
       end
     end
 
-    def serialize
-      "#{data_type}:#{@data}"
+    private
+
+    def read_next_entity
+      size   = read_next_record.to_i
+      entity = @data[0...size]
+
+      @data = @data[size...@data.size]
+
+      entity
     end
 
-    def self.parse(string_data)
-      data_type, data = string_data.split(':', 2)
-      data = case data_type
-               when 'string'  then data
-               when 'symbol'  then data.to_sym
-               when 'number'  then data.to_f
-               when 'boolean' then data == 'true'
-             end
-      File.new(data)
+    def read_next_record
+      record, @data = @data.split(':', 2)
+
+      record
     end
   end
 
   class Directory
-    def initialize
-      @directory = Hash.new
+    attr_reader :files, :directories
+
+    def initialize(files={}, directories={})
+      @files       = files
+      @directories = directories
     end
 
     def add_file(name, file)
-      @directory[name] = file
+      @files[name] = file
     end
 
-    def add_directory(name, directory = Directory.new)
-      @directory[name] = directory
+    def add_directory(name, directory=Directory.new)
+      @directories[name] = directory
     end
 
     def [](name)
-      @directory[name]
+      @directories[name] || @files[name]
     end
 
-    def files
-      @directory.select do |key, value|
-        value.is_a? File
-      end
+    def serialize
+      files       = "#{@files.size}:#{serialize_entities(@files)}"
+      directories = "#{@directories.size}:#{serialize_entities(@directories)}"
+
+      "#{files}#{directories}"
     end
 
-    def directories
-      @directory.select do |key, value|
-        value.is_a? Directory
+    def self.parse(string_data)
+      parser = Parser.new(string_data)
+
+      files       = {}
+      directories = {}
+
+      parser.each { |name, entity| files[name]       = File.parse(entity)      }
+      parser.each { |name, entity| directories[name] = Directory.parse(entity) }
+
+      Directory.new(files, directories)
+    end
+
+    private
+
+    def serialize_entities(entities)
+      entities.map do |name, entity|
+        serialized_entity = entity.serialize
+
+        "#{name}:#{serialized_entity.size}:#{serialized_entity}"
+      end.join('')
+    end
+  end
+
+  class File
+    attr_accessor :data
+
+    def initialize(data=nil)
+      @data = data
+    end
+
+    def data_type
+      case @data
+      when nil     then :nil
+      when String  then :string
+      when Symbol  then :symbol
+      when Numeric then :number
+      else              :boolean
       end
     end
 
     def serialize
-      string_data = "#{files.length}:"
-      files.each do |key, value|
-        string_data += "#{key}:#{value.serialize.length}:" + value.serialize
+      "#{data_type}:#{@data.to_s}"
+    end
+
+    def self.parse(string)
+      data = parse_data *string.split(':', 2)
+
+      File.new data
+    end
+
+    private
+
+    def self.parse_data(type, data)
+      case type
+      when 'nil'    then nil
+      when 'string' then data
+      when 'symbol' then data.to_sym
+      when 'number' then parse_number(data)
+      else               data == 'true'
       end
-      string_data += "#{directories.length}:"
-      directories.each do |key, value|
-        string_data += "#{key}:#{value.serialize.length}:" + value.serialize
+    end
+
+    def self.parse_number(data)
+      if data.include? '.'
+        data.to_f
+      else
+        data.to_i
       end
-      string_data
     end
   end
 end
